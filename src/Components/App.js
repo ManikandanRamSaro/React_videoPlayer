015ce78892799e1c0b2d89b2b40bbb3d64687352
ManikandanRamSaro <mani95ram@outlook.com>
2021-01-06 10:36:28 +0530

    Stored values of youtube videos into state - 6-1-21

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,17 +3,20 @@ import SearchBar from './SearchBar';
 import youtube from '../Apis/youtube'
 class App extends React.Component{
 
+    state = { videos : [] }
     getFromSearchbox = async (data) =>
     {
       const output= await youtube.get('/search',{
             params:{
                 q:data
             }
-        }) 
+        })
+        this.setState({videos:output.data.items}) 
     }
     render(){
         return(<div className="ui container">
             <SearchBar fromSearch={this.getFromSearchbox}/>
+            <p> we found {this.state.videos.length} list of videos </p>
         </div>);
     }
 }
